586e6cb957f837425be39b1acc6b182cd9349d93
Vinicius Campos <viniciuscampos120@gmail.com>
2018-08-25 23:48:23 -0300

    Update IBank interface

--- BankRMI/src/interfaces/IBank.java
+++ BankRMI/src/interfaces/IBank.java
@@ -1,11 +1,12 @@
 package interfaces;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface IBank extends Remote{
-    public Long createAccount(Long id, String password);
-    public Boolean deposit(Double value, Long account);
-    public Boolean withdraw(Long account, String password, Double value);
-    public Boolean transfer(Long account, String password, Double value, Long anotherAccount);
-    public String statement(Long account, String password);	
+    public Long createAccount(Long id, String password) throws RemoteException;
+    public Boolean deposit(Double value, Long account) throws RemoteException;
+    public Boolean withdraw(Long account, String password, Double value) throws RemoteException;
+    public Boolean transfer(Long account, String password, Double value, Long anotherAccount) throws RemoteException;
+    public String statement(Long account, String password) throws RemoteException;	
 }
